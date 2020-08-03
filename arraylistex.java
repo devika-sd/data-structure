f5d94d143b41855340533b6c178b37015dbc2a99
devika-sd <devikajeevaraj1310@gmail.com>
2020-08-03 21:05:43 +0530

    arraylist_add

---
+++ arraylistex.java
@@ -0,0 +1,12 @@
+import java.util.*;  
+ class ArrayListEx{  
+ public static void main(String args[]){  
+  ArrayList<String> list=new ArrayList<String>();//Creating arraylist    
+      list.add("friend");//Adding object in arraylist    
+      list.add("is");    
+      list.add("god's");    
+      list.add("blessing");    
+      //Printing the arraylist object   
+      System.out.println(list);  
+ }  
+} 
